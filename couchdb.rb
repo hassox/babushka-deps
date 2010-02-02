a0fbba39777266e4dab708bb53dcaa6ff5af95e2
Daniel Neighman <has.sox@gmail.com>
2010-02-02 22:48:53 +1100

    Remove the autoconf stuff

--- couchdb.rb
+++ couchdb.rb
@@ -1,5 +1,5 @@
 src 'couchdb' do
-  requires 'help2man', 'libtool', 'autoconf', 'automake',
+  requires 'help2man' #, 'libtool', 'autoconf', 'automake',
            'erlang-dev', 'erlang-nox', 'libicu-dev', 'libmozjs-dev', 'libcurl4-openssl-dev'
   source 'git://git.apache.org/couchdb.git'
   preconfigure do
